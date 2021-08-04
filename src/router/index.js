798adcd8c2f12539fa0507d5977a270aa6ffcd24
ErrolLin <errolriccolin@foxmail.com>
2021-08-04 10:50:24 +0800

    Update router

--- src/router/index.js
+++ src/router/index.js
@@ -1,17 +1,27 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import Home from '../views/Home.vue'
+import { createRouter, createWebHistory } from 'vue-router';
 
 const routes = [
   {
     path: '/',
-    name: 'Home',
-    component: Home
+    redirect: '/products',
   },
-]
+  {
+    path: '/products',
+    component: () => import('../views/ProductsList.vue'),
+  },
+  {
+    path: '/cart',
+    component: () => import('../views/UserCart.vue'),
+  },
+  {
+    path: '/admin',
+    component: () => import('../views/ShopAdmin.vue'),
+  },
+];
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
-  routes
-})
+  routes,
+});
 
-export default router
+export default router;
