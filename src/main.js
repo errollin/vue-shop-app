48d7a6dfb60116595ed77cdbc29914d63b2f2312
ErrolLin <errolriccolin@foxmail.com>
2021-08-04 10:52:30 +0800

    Add global component

--- src/main.js
+++ src/main.js
@@ -3,7 +3,10 @@ import App from './App.vue';
 import router from './router';
 import store from './store';
 
+import BaseBadge from './components/ui/BaseBadge.vue';
+
 const app = createApp(App);
+app.component('base-badge', BaseBadge);
 
 app.use(store);
 app.use(router);
